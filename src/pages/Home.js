0584fa4b273b703af5446ce8a2cd4b2d867bc851
whadesb <thomas.bouez@gmail.com>
2022-11-09 16:14:12 +0100

    debut app techno react

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h1>Work in progress</h1>
+            <div>Welcolm to the second app React</div>
+        </div>
+    )
+}
